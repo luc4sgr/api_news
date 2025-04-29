fdf745fbd86d267525f07131fbbee2bc3a1ec6fc
luc4sgr <23.lucasdoliveira@gmail.com>
2025-04-29 20:16:25 -0300

    feat: add pagination to comments endpoint

--- src/controllers/comment_controller.py
+++ src/controllers/comment_controller.py
@@ -1,6 +1,7 @@
 from flask import request, jsonify
 from src.services.comment_service import CommentService
 from src.validators.comment_validator import validate_create_comment
+from src.utils.paginator import paginate
 
 class CommentController:
     def __init__(self):
@@ -11,6 +12,12 @@ class CommentController:
         comment = self.comment_service.create_comment(**data)
         return jsonify(comment.to_dict()), 201
 
-    def list(self, news_id):
+    def list(self, news_id: str):
+        params = request.args.to_dict(flat=True)
         comments = self.comment_service.list_comments(news_id=news_id)
-        return jsonify([comment.to_dict() for comment in comments]), 200
+
+        page = int(params.get("page", 1))
+        per_page = int(params.get("per_page", 10))
+
+        paginated = paginate([c.to_dict() for c in comments], page, per_page)
+        return jsonify(paginated), 200
