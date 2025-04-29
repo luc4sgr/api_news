d64fe7a54ef381743f32f7761331deeed4d6d051
luc4sgr <23.lucasdoliveira@gmail.com>
2025-04-29 16:13:27 -0300

    feat: create services

---
+++ src/services/news_service.py
@@ -0,0 +1,22 @@
+from src.models.news import News
+
+class NewsService:
+    def __init__(self):
+        self.news: list[News] = []
+    
+    def create_news(self, author_id:int, category:str, content:str, title:str="") -> News:
+        news_id = len(self.news) + 1
+        news = News(news_id=news_id, author_id=author_id,category=category, content=content, likes=0, title=title )
+        self.news.append(news)
+        return news
+    
+    def news_list(self) -> list[News]:
+        return self.news
+    
+    def like_news(self, news_id: int):
+        for news in self.news:
+            if news.news_id == news_id:
+                news.likes += 1
+                return news
+        return None
+
