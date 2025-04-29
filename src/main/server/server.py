186e78beac92cb990983d32182be9e9ab1a228db
luc4sgr <23.lucasdoliveira@gmail.com>
2025-04-29 16:29:04 -0300

    refactor:add comment route

--- src/main/server/server.py
+++ src/main/server/server.py
@@ -2,8 +2,10 @@ from flask import Flask
 from src.main.routes.user_routes import user_bp
 from src.main.routes.category_routes import category_bp
 from src.main.routes.news_routes import news_bp
+from src.main.routes.comment_routes import comment_bp
 
 app = Flask(__name__)
 app.register_blueprint(user_bp)
 app.register_blueprint(category_bp)
 app.register_blueprint(news_bp)
+app.register_blueprint(comment_bp)
